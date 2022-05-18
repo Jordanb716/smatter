3f80bbd48e4f66a772cc28ee7180d4d039db86bf
Jordan Bergmann <jordanb716@gmail.com>
2022-05-18 09:31:41 -0700

    Add system to zoom camera with mouse scrollwheel Close #16

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,27 @@ mod simulation;
 fn main() {
 	App::new()
 		.add_plugins(DefaultPlugins)
-		
 		// Spawning
 		.add_startup_system(simulation::camera::setup_camera)
 		.add_startup_system(simulation::spawning::spawn_player_ship)
-
 		// Camera
 		.add_system(simulation::camera::camera_follow_player)
-
+		.add_system(simulation::camera::camera_zoom_system)
 		// Physics
 		.add_system(simulation::physics::object_movement_system)
 		.add_system(simulation::physics::velocity_calculation_system)
-
 		//Interaction
 		.add_system(simulation::physics::projectile_collision_system)
 		.add_system(simulation::interaction::kill_system)
-
 		//Spawn turrets and guns
 		.add_system(simulation::turret_list::ship_turret_spawn_system)
 		.add_system(simulation::gun_list::turret_gun_spawn_system)
-
 		//Turrets and guns
 		.add_system(simulation::targeting::turret_target_selection)
 		.add_system(simulation::targeting::turret_targeting_system)
 		.add_system(simulation::gun::gun_firing_system)
-
 		// More spawning
 		.add_system(simulation::spawning::target_spawn_system)
 		.insert_resource(simulation::SpawnTimer(Timer::from_seconds(0.5, true)))
-
 		.run();
 }
--- src/simulation/camera.rs
+++ src/simulation/camera.rs
@@ -19,7 +19,33 @@ pub fn camera_follow_player(
 	player_ship: Query<&Transform, (With<ship::IsPlayerShip>, Without<IsCamera>)>,
 ) {
 	let camera_target = player_ship.single().translation;
-	let mut camera = camera.get_single_mut().unwrap();
+	let mut camera = camera
+		.get_single_mut()
+		.expect("Camera follow broke on getting the camera");
 	camera.translation.x = camera_target.x;
 	camera.translation.y = camera_target.y;
+}
+
+/// Camera Zoom System
+/// Lets the mouse scrool wheel control camera zoom
+pub fn camera_zoom_system(
+	mut mouse_wheel_events: EventReader<bevy::input::mouse::MouseWheel>,
+	mut camera: Query<&mut OrthographicProjection, With<IsCamera>>,
+) {
+	const CAMERA_ZOOM_SENSITIVITY: f32 = 2.0;
+	const CAMERA_SCALE_MAX: f32 = 2000.0; // Zoomed all the way out
+	const CAMERA_SCALE_MIN: f32 = 200.0; // Zoomed all the way in
+
+	for mouse_wheel_event in mouse_wheel_events.iter() {
+		let mut camera = camera
+			.get_single_mut()
+			.expect("Zoom broke on getting the camera");
+		let dy = match mouse_wheel_event.unit {
+			bevy::input::mouse::MouseScrollUnit::Line => mouse_wheel_event.y * 20.,
+			bevy::input::mouse::MouseScrollUnit::Pixel => mouse_wheel_event.y,
+		};
+		camera.scale -= dy * CAMERA_ZOOM_SENSITIVITY;
+		camera.scale = camera.scale.clamp(CAMERA_SCALE_MIN, CAMERA_SCALE_MAX);
+		dbg!(camera.scale);
+	}
 }
